94180a0a224954273ffc87a1882e0bf3d2e666f1
mbron770 <mbronfin.tsd@gmail.com>
2023-05-11 19:11:59 -0400

    complete index.html page and set global background image

--- script.js
+++ script.js
@@ -1,6 +1,78 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+/*document.body.onload = showIndexWelcomeText;
 
-document.querySelector("body").appendChild(h2);
+function showIndexWelcomeText1() {
+    const welcomeParagraph = document.createElement("h1");
+    welcomeParagraph.setAttribute("id", "welcomeParagraph");
+    const welcomeParagraphText = document.createTextNode("I am Mordechai Bronfin" + "\nFull Stack Developer. AWS Solutions Architect. The Flatiron School Student.");
+    welcomeParagraph.appendChild(welcomeParagraphText);
+   /* welcomeParagraph.appendChild("br");
+    welcomeParagraph.appendChild("Full Stack Developer. AWS Solutions Architect. The Flatiron School Student.");
+
+    document.getElementById("indexWelcomeTextDiv").appendChild(welcomeParagraph);
+   
+}*/
+
+
+
+function showIndexWelcomeTextH1() {
+  const welcomeParagraph1 = document.createElement("h1");
+  welcomeParagraph1.setAttribute("id", "welcomeParagraph1");
+  welcomeParagraph1.innerHTML = "I am Mordechai Bronfin";
+  document.getElementById("indexWelcomeTextDiv").appendChild(welcomeParagraph1);
+}
+
+function showIndexWelcomeTextH2() {
+  const welcomeParagraph2 = document.createElement("h2");
+  welcomeParagraph2.setAttribute("id", "welcomeParagraph2");
+  welcomeParagraph2.innerHTML = "Full Stack Developer. AWS Solutions Architect. The Flatiron School Student.";
+  document.getElementById("indexWelcomeTextDiv").appendChild(welcomeParagraph2);
+}
+
+function addImageToIndex() {
+  const imageTag = document.createElement("img");
+  imageTag.setAttribute("src", "images/myImage.JPG");
+  document.getElementById("indexWelcomeTextDiv").appendChild(imageTag);
+}
+
+function addAboutParagraph1() {
+  const aboutParagraph1 = document.createElement("p");
+  aboutParagraph1.setAttribute("id", "aboutParagraph1");
+  aboutParagraph1.innerHTML = "Welcome to my personal website and portfolio! I made this website from scratch using the skills I learned from the Flatiron prep course as well as from my previous work. This website is meant to showcase my skills and experience and practice full stack web development.";
+  document.getElementById("indexWelcomeTextDiv").appendChild(aboutParagraph1);
+}
+
+function addAboutParagraph2() {
+  const aboutParagraph2 = document.createElement("p");
+  aboutParagraph2.setAttribute("id", "aboutParagraph2");
+  aboutParagraph2.innerHTML = "";
+  document.getElementById("indexWelcomeTextDiv").appendChild(aboutParagraph2);
+}
+
+
+
+
+
+
+
+showIndexWelcomeTextH1();
+showIndexWelcomeTextH2(); 
+addImageToIndex(); 
+addAboutParagraph1(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
